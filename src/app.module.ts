abc9eec1fdeb61eb63574b6d0d7b29b9e87f8c7b
Suganov Boris <suganov@t8.ru>
2024-11-22 23:00:12 +0300

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getDatabaseConfig } from './config/database.config';
+import { validationSchema } from './config/validation.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [],
+  imports: [
+    // Подключение ConfigModule с валидацией
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema,
+    }),
+
+    // Подключение TypeOrmModule
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        getDatabaseConfig(configService),
+    }),
+  ],
 })
 export class AppModule {}
